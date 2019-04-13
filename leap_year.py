de543922b4cf59b0ecd7e2e4a128457fff35e6e1
Steve Porter <sjporter399@gmail.com>
2019-04-12 19:58:07 -0600

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -5,4 +5,14 @@
 # Don't forget to reach out for help after your own due diligence
 
 def is_leap_year(year):
-    return ''
+    if year % 4 == 0:
+        if year % 100 != 0:
+            return True
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+    else:
+        return False
